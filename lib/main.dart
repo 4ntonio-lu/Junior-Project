12b8fe2aef4a77417147901eac3b64eba264c362
Jason Wells <jfrostwells@gmail.com>
2022-11-08 16:03:13 -0800

    Notification button working!!!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +13,19 @@ import 'notifications.dart';
 //part 'index.g.dart';
 
 void main() async {
-  runApp(MyApp());
+  AwesomeNotifications().initialize(
+      null,
+      [
+      NotificationChannel(
+      channelKey: 'basic_channel',
+      channelName: 'Basic notif',
+  channelDescription: 'hey',
+      importance: NotificationImportance.High,
+      channelShowBadge: true,
+  )
+  ]
+  );
+      runApp(MyApp());
   print("ok");
   String reminders = await idk();
   print(reminders);
@@ -54,7 +67,7 @@ Future<String> idk() async {
     final s = await file.readAsString();
     return s;
   } catch (e) {
-  // If encountering an error, return 0
+    // If encountering an error, return 0
     return "no";
   }
 }
@@ -160,55 +173,133 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: createNotification, //_incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  @override
+  void initState() {
+    super.initState();
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        showDialog(context: context,
+            builder: (context) =>
+                AlertDialog(title: Text('Allow Notifications'),
+                    content: Text('Our app would like to send notifications.'),
+                    actions: [TextButton(onPressed: () {
+                      Navigator.pop(context);
+                    }, child: Text(
+                      'Dont allow',
+                      style: TextStyle(color: Colors.amber, fontSize: 18,
+                      ),
+                    ),
+                    ),
+                      TextButton(
+                          onPressed: () =>
+                              AwesomeNotifications()
+                                  .requestPermissionToSendNotifications()
+                                  .then((_) => Navigator.pop(context)),
+                          child: Text(
+                              'Allow',
+                              style: TextStyle(color: Colors.teal, fontSize: 18,
+                                fontWeight: FontWeight.bold,)
+                          ))
+                    ]
+                )
 
+        );
+      }
+
+      @override
+      Widget build(BuildContext context) {
+        // This method is rerun every time setState is called, for instance as done
+        // by the _incrementCounter method above.
+        //
+        // The Flutter framework has been optimized to make rerunning build methods
+        // fast, so that you can just rebuild anything that needs updating rather
+        // than having to individually change instances of widgets.
+        return Scaffold(
+          appBar: AppBar(
+            // Here we take the value from the MyHomePage object that was created by
+            // the App.build method, and use it to set our appbar title.
+            title: Text(widget.title),
+          ),
+          body: Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Invoke "debug painting" (press "p" in the console, choose the
+              // "Toggle Debug Paint" action from the Flutter Inspector in Android
+              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+              // to see the wireframe for each widget.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .headline4,
+                ),
+              ],
+            ),
+          ),
+
+
+          floatingActionButton: FloatingActionButton(
+            onPressed: createNotification, //_incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+
+        );
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+
+        title: Text(widget.title),
+    ),
+    body: Center(
+
+    child: Column(
+
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+    const Text(
+    'You have pushed the button this many times:',
+    ),
+    Text(
+    '$_counter',
+    style: Theme
+        .of(context)
+        .textTheme
+        .headline4,
+    ),
+    ],
+    ),
+    ),
+
+
+    floatingActionButton: FloatingActionButton(
+    onPressed: createNotification, //_incrementCounter,
+    tooltip: 'Increment',
+    child: const Icon(Icons.add),
+    )
     );
   }
 }
